6776687a725c26d6ad04c53f19763ff919b04966
Daniel Perrefort <djperrefort@gmail.com>
2024-03-11 15:23:51 -0400

    Adds basic health check data to landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { ApiService } from "./core/api.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  apiVersion = "N/A"
+  apiVersion: string = ""
+  healthData: any[] = []
 
-  constructor(private apiService: ApiService) {
-  }
+  constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
     this.fetchVersion();
+    this.fetchHealthChecks();
   }
 
   fetchVersion(): void {
@@ -29,5 +29,16 @@ export class AppComponent implements OnInit {
         console.error('Error fetching API version:', error);
       }
     });
+  }
+
+  fetchHealthChecks(): void {
+    this.apiService.healthGet().subscribe({
+      next: (data) => {
+        this.healthData = data.message;
+      },
+      error: (data) => {
+        this.healthData = data.error;
+      }
+    })
   }
 }
