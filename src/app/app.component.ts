c510c1dff889503d98921398a5c8bc46dc9abe78
Daniel Perrefort <djperrefort@gmail.com>
2024-03-10 23:10:27 -0400

    Adds simple http get logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { environment} from "../environments/environment";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'keystone';
+export class AppComponent implements OnInit {
+  httpClient = inject(HttpClient)
+
+  title: string = "Keystone";
+
+  ngOnInit(): void {
+    this.getAPIVersion()
+  }
+
+  getAPIVersion(): void {
+    const endpoint = environment.API_URL + '/version/'
+    console.log(endpoint)
+    this.httpClient.get(endpoint)
+      .subscribe(
+        (data) => {
+          console.log(data);
+        }
+      )
+  }
 }
