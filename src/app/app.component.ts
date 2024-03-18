26a873b73ba595965c0861cd44608dc587b15e45
Daniel Perrefort <djperrefort@gmail.com>
2024-03-17 22:24:18 -0400

    Restore components from main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,14 @@
 import { Component } from '@angular/core';
-import { ApiService } from "./core/api.service";
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-
-  constructor(private apiService: ApiService) {
-    this.apiService.login('admin', 'admin')
-    this.apiService.get('health/').subscribe({
-      next: (response) => console.log(response),
-      error: (error) => console.log(error.error)
-    })
-  }
+  title = 'keystone';
 }
