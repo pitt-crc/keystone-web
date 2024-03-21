e98c1097bb3826990a9dbe9e61ca2675c0dbcf0a
Daniel Perrefort <djperrefort@gmail.com>
2024-03-21 14:36:45 -0400

    Adds placeholder page for the keystone dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
-import {LoginComponent} from "./authentication/components/login/login.component";
+import { LoginComponent } from "./authentication/components/login/login.component";
+import { DashboardComponent } from "./dashboard/components/dashboard/dashboard.component";
 
 export const routes: Routes = [
+    { path: '', component: DashboardComponent },
     { path: 'login', component: LoginComponent },
 ];
