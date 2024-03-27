4e69b939a86ad78d9ab710b72daf88c83d40ca49
Daniel Perrefort <djperrefort@gmail.com>
2024-03-27 11:45:33 -0400

    Adds component for 404 errors

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from "./authentication/components/login/login.componen
 import { LogoutComponent } from "./authentication/components/logout/logout.component";
 import { DashboardComponent } from "./dashboard/components/dashboard/dashboard.component";
 import { authGuard } from "./authentication/guards/authGuard";
+import { PageNotFoundComponent } from "./common/components/page-not-found/page-not-found.component";
 
 export const routes: Routes = [
     {path: '', component: DashboardComponent, canActivate: [authGuard]},
@@ -10,4 +11,5 @@ export const routes: Routes = [
         {path: 'login', component: LoginComponent},
         {path: 'logout', component: LogoutComponent},
     ]},
+    {path: '**', component: PageNotFoundComponent},
 ];
---
+++ src/app/common/components/page-not-found/page-not-found.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found',
+  standalone: true,
+  templateUrl: './page-not-found.component.html',
+})
+export class PageNotFoundComponent {}
