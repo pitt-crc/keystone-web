5fa43bc39cb0392a5f90d0cdd72ebf5c92fdc335
Daniel Perrefort <djperrefort@gmail.com>
2024-04-02 16:08:41 -0400

    Makes logout status dynamic

--- src/app/authentication/components/logout/logout.component.ts
+++ src/app/authentication/components/logout/logout.component.ts
@@ -7,14 +7,19 @@ import { ApiService } from "../../../common/services/api.service";
   templateUrl: 'logout.component.html',
 })
 export class LogoutComponent implements OnInit {
+  statusText = 'You are being logged out...';
 
   constructor(private apiService: ApiService) {}
 
   /**
-   * Automatically log the user out on page loa
+   * Automatically log the user out on page load
    */
   ngOnInit(): void {
-    this.apiService.logout().subscribe();
+    this.apiService.logout().subscribe({
+      next: () => {
+        this.statusText = 'You have been successfully logged out.';
+      }
+    });
   }
 
 }
