948c1136f8c4e0a0c5e3dbdffa61647567258b41
Daniel Perrefort <djperrefort@gmail.com>
2024-04-04 15:35:23 -0400

    Updates routing and metadata

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
+
+import { AuthComponent } from "./authentication/components/auth/auth.component";
+import { authGuard } from "./authentication/guards/authGuard";
+import { DashboardLayoutComponent } from "./dashboard/components/dashboardLayout/dashboardLayout.component";
 import { LoginComponent } from "./authentication/components/login/login.component";
 import { LogoutComponent } from "./authentication/components/logout/logout.component";
-import { DashboardLayoutComponent } from "./dashboard/components/dashboardLayout/dashboardLayout.component";
-import { authGuard } from "./authentication/guards/authGuard";
 import { PageNotFoundComponent } from "./common/components/page-not-found/page-not-found.component";
-import { AuthComponent } from "./authentication/components/auth/auth.component";
 
 export const routes: Routes = [
-    {path: '', component: DashboardLayoutComponent, canActivate: [authGuard]},
+    {path: '', redirectTo: 'app', pathMatch: 'full' },
+    {path: 'dashboard', component: DashboardLayoutComponent, canActivate: [authGuard]},
     {path: 'auth', component: AuthComponent, children: [
         {path: 'login', component: LoginComponent},
         {path: 'logout', component: LogoutComponent},
