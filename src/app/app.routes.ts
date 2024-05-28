61efae460016dd7c4dbae5f0657b3990e872c7f5
Daniel Perrefort <djperrefort@gmail.com>
2024-05-28 14:00:10 -0400

    Moves auth guard to common module (#30)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 
 import { AuthLayoutComponent } from "./authentication/components/auth-layout/auth-layout.component";
-import { authGuard } from "./authentication/guards/authGuard";
-import { DashboardLayoutComponent } from "./dashboard/components/dashboardLayout/dashboardLayout.component";
 import { LoginComponent } from "./authentication/components/login/login.component";
 import { LogoutComponent } from "./authentication/components/logout/logout.component";
 import { PageNotFoundComponent } from "./common/components/page-not-found/page-not-found.component";
+import { authGuard } from "./common/guards/authGuard";
+import { DashboardLayoutComponent } from "./dashboard/components/dashboardLayout/dashboardLayout.component";
 
 export const routes: Routes = [
     {path: '', redirectTo: 'dashboard', pathMatch: 'full' },
