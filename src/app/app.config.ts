c50847afeb33b86e6a7a94554394ddc0e168f230
djperrefort <djperrefort@gmail.com>
2024-05-29 18:38:36 -0400

    Adds placeholder links

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,5 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { BootstrapIconsModule } from 'ng-bootstrap-icons';
-import { allIcons } from 'ng-bootstrap-icons/icons';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
@@ -9,6 +7,5 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(withFetch()),
     provideRouter(routes),
-    importProvidersFrom(BootstrapIconsModule.pick(allIcons))
   ]
 };
--- src/app/dashboard/components/sidebar/sidebar.component.ts
+++ src/app/dashboard/components/sidebar/sidebar.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
-import { BootstrapIconsModule } from "ng-bootstrap-icons";
 import { KeyValuePipe, NgForOf } from "@angular/common";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [BootstrapIconsModule, KeyValuePipe, NgForOf],
+  imports: [KeyValuePipe, NgForOf],
   templateUrl: 'sidebar.component.html',
 })
 export class SidebarComponent {
   navStructure: NavStructure = {
     Overview: {
-      Dashboard: {href: 'dashboard', icon: 'alarm'},
-      Example: {href: 'settings', icon: 'app'},
-      Hello: {href: 'settings', icon: 'github'}
+      Dashboard: {href: 'dashboard', icon: 'speedometer'},
+    },
+    Resources: {
+      Allocations: {href: 'resources/allocations', icon: 'cpu'},
+      Requests: {href: 'resources/requests', icon: 'arrow-clockwise'},
+      Usage: {href: 'resources/usage', icon: 'bar-chart'},
+    },
+    Research: {
+      Publications: {href: 'research/publications', icon: 'journal-text'},
+      Grants: {href: 'research/grants', icon: 'coin'},
     }
   };
 }
