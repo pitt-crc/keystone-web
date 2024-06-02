dc5e006be6f8dcb18a55739a9af2f330ebcfcdb2
djperrefort <djperrefort@gmail.com>
2024-06-02 09:43:42 -0400

    Dynamically render sidebar nav links

--- src/app/dashboard/components/dashboardLayout/dashboardLayout.component.ts
+++ src/app/dashboard/components/dashboardLayout/dashboardLayout.component.ts
@@ -1,40 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
-
-import { environment } from '../../../../environments/environment';
-import { KeyValuePipe, NgForOf } from "@angular/common";
 import { TopNavComponent } from "../topNav/topNav.component";
 import { SideNavComponent } from "../sideNav/sideNav.component";
+
 @Component({
   selector: 'app-dashboardLayout',
   standalone: true,
-  imports: [RouterOutlet, KeyValuePipe, NgForOf, TopNavComponent, SideNavComponent],
+  imports: [RouterOutlet, TopNavComponent, SideNavComponent],
   templateUrl: 'dashboardLayout.component.html',
   styleUrl: 'dashboardLayout.component.scss'
 })
-export class DashboardLayoutComponent {
-  version = environment.version;
-  navStructure: NavStructure = {
-    Overview: {
-      Dashboard: {href: 'dashboard', icon: 'speedometer'},
-    },
-    Resources: {
-      Allocations: {href: 'resources/allocations', icon: 'cpu'},
-      Requests: {href: 'resources/requests', icon: 'arrow-clockwise'},
-      Usage: {href: 'resources/usage', icon: 'bar-chart'},
-    },
-    Research: {
-      Publications: {href: 'research/publications', icon: 'journal-text'},
-      Grants: {href: 'research/grants', icon: 'coin'},
-    }
-  };
-}
-
-interface NavStructure {
-  [key: string]: {
-    [subKey: string]: {
-      href: string;
-      icon: string;
-    }
-  };
-}
+export class DashboardLayoutComponent {}
--- src/app/dashboard/components/sideNav/sideNav.component.ts
+++ src/app/dashboard/components/sideNav/sideNav.component.ts
@@ -1,13 +1,50 @@
 import { Component } from '@angular/core';
 
 import { environment } from "../../../../environments/environment";
+import { KeyValuePipe, NgForOf } from "@angular/common";
 
 @Component({
   selector: 'app-sideNav',
   standalone: true,
+  imports: [KeyValuePipe, NgForOf],
   templateUrl: 'sideNav.component.html',
   styleUrl: 'sideNav.component.scss'
 })
 export class SideNavComponent {
   version: string = environment.version;
+  navStructure: NavSection[] = [
+    {
+      title: 'Overview',
+      links: {
+        Dashboard: {href: 'dashboard', icon: 'speedometer'},
+      }
+    },
+    {
+      title: 'Resources',
+      links: {
+        Allocations: {href: 'resources/allocations', icon: 'cpu'},
+        Requests: {href: 'resources/requests', icon: 'arrow-clockwise'},
+        Usage: {href: 'resources/usage', icon: 'bar-chart'},
+      }
+    },
+    {
+      title: 'Research',
+      links: {
+        Publications: {href: 'research/publications', icon: 'journal-text'},
+        Grants: {href: 'research/grants', icon: 'coin'},
+      }
+    }
+  ];
+}
+
+interface NavLink {
+  href: string;
+  icon: string;
+}
+
+interface NavSection {
+  title: string;
+  links: {
+    [key: string]: NavLink;
+  };
 }
